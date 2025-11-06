2c98b984c660a70e0d7134b6d942e72938aeb06b
Flechazo <2558755403@qq.com>
2025-11-07 01:13:28 +0800

    init 1.21.10

--- Fabric/src/main/java/com/yungnickyoung/minecraft/ribbits/config/ConfigGeneralFabric.java
+++
@@ -1,8 +0,0 @@
-package com.yungnickyoung.minecraft.ribbits.config;
-
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
-
-public class ConfigGeneralFabric {
-    @ConfigEntry.Gui.Tooltip
-    public boolean prideFlagAllYear = false;
-}
--- NeoForge/src/main/java/com/yungnickyoung/minecraft/ribbits/block/GiantLilyPadBlockNeoForge.java
+++
@@ -1,7 +0,0 @@
-package com.yungnickyoung.minecraft.ribbits.block;
-
-public class GiantLilyPadBlockNeoForge extends GiantLilyPadBlock {
-    public GiantLilyPadBlockNeoForge(Properties properties) {
-        super(properties);
-    }
-}
